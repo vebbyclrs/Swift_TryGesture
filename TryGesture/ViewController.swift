cba29e65b0f2f09b24ec023e0de743693a3d067b
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-07-11 14:16:58 +0700

    Initial Commit

---
+++ TryGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryGesture
+//
+//  Created by Vebby Clarissa on 11/07/19.
+//  Copyright © 2019 Geomecin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
